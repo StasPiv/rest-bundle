9e7007960cff58238029d17de821ac5eb64f4d93
Stanislav Pivovartsev <ssp@nxc.no>
2016-09-07 17:14:16 +0300

    make it bundle

---
+++ src/RestBundle.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by rest-bundle.
+ * User: ssp
+ * Date: 07.09.16
+ * Time: 17:13
+ */
+
+namespace NorseDigital\Symfony\RestBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class RestBundle extends Bundle
+{
+
+}
