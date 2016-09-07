7c73e770e19d41853830b3528fea42e33efaaf2f
Stanislav Pivovartsev <ssp@nxc.no>
2016-09-07 17:14:34 +0300

    Make it bundle.

--- src/RestBundle.php
+++ src/RestBundle.php
@@ -3,14 +3,12 @@
  * Created by rest-bundle.
  * User: ssp
  * Date: 07.09.16
- * Time: 17:13
+ * Time: 17:13.
  */
-
 namespace NorseDigital\Symfony\RestBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class RestBundle extends Bundle
 {
-
 }
