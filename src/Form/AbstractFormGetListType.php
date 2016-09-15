9735f67cfc09bea70ec5d2d3e8e1fafa949cb714
Stanislav Pivovartsev <ssp@nxc.no>
2016-09-15 12:00:34 +0300

    Add AbstractFormGetListType

---
+++ src/Form/AbstractFormGetListType.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: in
+ * Date: 26.07.16
+ * Time: 11:40.
+ */
+namespace NorseDigital\Symfony\RestBundle\Form;
+
+use Doctrine\Common\Collections\Criteria;
+use NorseDigital\Symfony\RestBundle\Request\ListRequestInterface;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * Class AbstractFormGetListTyp.
+ */
+abstract class AbstractFormGetListType extends AbstractFormType
+{
+    const DATA_CLASS = ListRequestInterface::class;
+
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+
+        $builder
+            ->add('limit', IntegerType::class, [
+                'required' => false,
+            ])
+            ->add('page', IntegerType::class, [
+                'required' => false,
+            ])
+            ->add('sort', TextType::class, [
+                'required' => false,
+            ])
+            ->add('order', ChoiceType::class, [
+                'required' => false,
+                'choices' => [
+                    Criteria::ASC,
+                    Criteria::DESC,
+                ],
+                'choices_as_values' => true,
+            ]);
+    }
+}
