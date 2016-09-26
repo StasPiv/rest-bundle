6862f1832b2651e11c674ac9f101b7386c769cd3
Stanislav Pivovartsev <ssp@nxc.no>
2016-09-26 18:04:49 +0300

    Create command for loading fixtures with truncate

---
+++ src/Command/LoadDataFixturesDoctrineCommand.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by cvident-backend-api.
+ * User: ssp
+ * Date: 26.09.16
+ * Time: 18:00.
+ */
+namespace NorseDigital\Symfony\RestBundle\Command;
+
+use Doctrine\Bundle\FixturesBundle\Command\LoadDataFixturesDoctrineCommand as BaseCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class LoadDataFixturesDoctrineCommand extends BaseCommand
+{
+    protected function configure()
+    {
+        parent::configure();
+        $this->setName('nd:symfony:doctrine:fixtures:load');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->getContainer()->get('doctrine')->getManager()->getConnection()->prepare('SET FOREIGN_KEY_CHECKS = 0;')->execute();
+        $result = parent::execute($input, $output);
+        $this->getContainer()->get('doctrine')->getManager()->getConnection()->prepare('SET FOREIGN_KEY_CHECKS = 1;')->execute();
+
+        return $result;
+    }
+}
